d075182eb2b71bdc1ca69d9f88fd89c5d07c84bf
ppankk <ppankk@gmail.com>
2018-12-03 01:48:05 +0200

    Try to use Lucene for regexp search. Now not working(

--- DAO/src/main/java/ua/ppankk/DAO/repository/impl/ContactDAO_impl.java
+++ DAO/src/main/java/ua/ppankk/DAO/repository/impl/ContactDAO_impl.java
@@ -1,5 +1,8 @@
 package ua.ppankk.DAO.repository.impl;
 
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.RegexpQuery;
+import org.hibernate.search.jpa.FullTextEntityManager;
 import org.springframework.stereotype.Repository;
 import ua.ppankk.DAO.model.Contact;
 import ua.ppankk.DAO.repository.ContactDAO;
@@ -7,11 +10,6 @@ import ua.ppankk.DAO.repository.ContactDAO;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.EntityType;
-import javax.persistence.metamodel.Metamodel;
 import java.util.List;
 
 @Repository
@@ -27,21 +25,20 @@ public class ContactDAO_impl implements ContactDAO {
 
     @Override
     public List<Contact> gelFilteredContacts(String regex) {
+        FullTextEntityManager fullTextEntityManager =
+                org.hibernate.search.jpa.Search.getFullTextEntityManager(entityManager);
+        entityManager.getTransaction().begin();
 
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        RegexpQuery luceneQuery = new RegexpQuery(new Term("name", regex));
+        javax.persistence.Query jpaQuery =
+                fullTextEntityManager.createFullTextQuery(luceneQuery, Contact.class);
 
-        CriteriaQuery<Contact> criteriaQuery = criteriaBuilder.createQuery(Contact.class);
-        Metamodel metamodel = entityManager.getMetamodel();
-        EntityType<Contact> contactEntityType = metamodel.entity(Contact.class);
-        Root<Contact> contactRoot = criteriaQuery.from(contactEntityType);
-        criteriaQuery.where(
-                criteriaBuilder.like(contactRoot.get("name"),regex)
-        );
-        criteriaQuery.select(contactRoot);
+        List result = jpaQuery.getResultList();
 
-        TypedQuery<Contact> query = entityManager.createQuery(criteriaQuery);
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
-        return query.getResultList();
+        return result;
     }
 
     @Override
